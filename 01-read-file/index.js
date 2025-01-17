37ba1ed317b3a4bb9e36efa10f09f037aac18484
Christina Kushneruk <krzysia13131@gmail.com>
2025-01-17 18:33:59 +0300

    feat: add solution to 01-task

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+function redingFile() {
+  const fs = require('fs');
+  const readableStream = fs.createReadStream(
+    './01-read-file/text.txt',
+    'utf-8',
+  );
+  readableStream.on('data', (chunk) => console.log(chunk));
+}
+
+redingFile();
